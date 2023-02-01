25ef8b25a1f4c3f0946dee8583788beea7cf8317
mr-ema <antonio.a.v.c2017@gmail.com>
2023-02-01 09:03:44 -0300

    fix(neovim): correct spelling of :TSUpdate in packer.lua

--- config/nvim/lua/kraken/packer.lua
+++ config/nvim/lua/kraken/packer.lua
@@ -17,7 +17,7 @@ return require('packer').startup(function(use)
         use 'lukas-reineke/indent-blankline.nvim'
 
         -- Treesitter
-        use { 'nvim-treesitter/nvim-treesitter', run = ':TSUdate' }
+        use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
         -- Status line
         use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
