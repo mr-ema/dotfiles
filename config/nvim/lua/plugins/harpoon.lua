5d7a05ebf88b0bfdbc6318035b4e80657cfde012
mr-ema <ema.a.v.c2017@gmail.com>
2023-04-17 15:29:05 -0400

    change `harpon` keymaps -> neovim
    - use [1h 2h 3h 4h] intead of [h1 h2 h3 h4]

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -5,10 +5,10 @@ return {
         { "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>" },
         { "<leader>h", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>" },
 
-        { "<leader>h1", function() require('harpoon.ui').nav_file(1) end },
-        { "<leader>h2", function() require('harpoon.ui').nav_file(2) end },
-        { "<leader>h3", function() require('harpoon.ui').nav_file(3) end },
-        { "<leader>h4", function() require('harpoon.ui').nav_file(4) end },
+        { "<leader>1h", function() require('harpoon.ui').nav_file(1) end },
+        { "<leader>2h", function() require('harpoon.ui').nav_file(2) end },
+        { "<leader>3h", function() require('harpoon.ui').nav_file(3) end },
+        { "<leader>4h", function() require('harpoon.ui').nav_file(4) end },
     },
 }
 
