ca590b831889969d47fd3b7cff22cfa59c7bdd5b
mr-ema <ema.a.v.c2017@gmail.com>
2023-04-23 12:15:31 -0400

    feat: add custom functions -> neovim
    - toggle_transparency() -> toggle the transparency in the tokyonight theme
    - set_ident_to(n) -> change the identation to 'n' default to '4'

--- config/nvim/lua/utils/init.lua
+++ config/nvim/lua/utils/init.lua
@@ -1,3 +1,23 @@
 -------------------------------------------------------
 -- HERE YOU CAN SOURCE ALL FILES THAT YOU WANNA LOAD --
 -------------------------------------------------------
+-- or you can put all your utils in one file
+
+local utils = {}
+
+--[[change 'tabstop','shiftwidth' and 'softtabstop' to n]]
+function utils.set_indent_to(n)
+        n = n or 4
+        vim.bo.tabstop = n             -- the width of a hard tabstop measured in "spaces"
+        vim.bo.softtabstop = n         -- the size of an "indent". It's also measured in spaces
+        vim.bo.shiftwidth = n          -- sets the number of columns for a TAB
+end
+
+--[[toggle transparency in tokyonight plugin]]
+function utils.toggle_transparency()
+        local theme = require("tokyonight.config").options
+        theme.transparent = not theme.transparent
+        vim.cmd("colorscheme tokyonight-" .. theme.style)
+end
+
+return utils
