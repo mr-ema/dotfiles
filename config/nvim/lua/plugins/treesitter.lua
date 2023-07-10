f9d3df630ea88981f6d06acc3d022db3dd5416a5
mr-ema <ema.a.v.c2017@gmail.com>
2023-07-10 15:19:10 -0400

    update `treesitter` configuration -> neovim

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 
     config = function()
         require("nvim-treesitter.configs").setup {
-            ensure_installed = { "help", "c", "lua", "rust", "javascript", "typescript" },
+            ensure_installed = { }, -- Leave empty to avoid error in first install
             sync_install = false,
             auto_install = false, -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 
