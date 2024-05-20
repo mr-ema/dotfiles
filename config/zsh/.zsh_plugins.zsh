5fcd4217eef98ac125f74b75c278c46b627a6b57
mr-ema <ema.a.v.c2017@gmail.com>
2024-05-19 22:33:18 -0400

    remove autogenerated antidote file -> zsh

--- config/zsh/.zsh_plugins.zsh
+++
@@ -1,3 +0,0 @@
-fpath+=( /home/kraken/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /home/kraken/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /home/kraken/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
