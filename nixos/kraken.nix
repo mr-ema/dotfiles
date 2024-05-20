7e0a293bd47fdbdc470e0fc44d48c0c24cf324cc
mr-ema <ema.a.v.c2017@gmail.com>
2024-05-20 17:47:34 -0400

    added nixos configuration

---
+++ nixos/kraken.nix
@@ -0,0 +1,50 @@
+# My custom nixos configuration you can import it in "etc/nixos/configuration.nix"
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Modules 
+      ./modules/fhs-env.nix
+    ];
+ 
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    openssh
+    glibc
+    neovim
+    wget
+    figlet
+    ffmpeg
+    fzf
+    atuin
+
+    zsh
+    bash
+
+    kitty
+
+    zip
+    unzip
+    gnutar
+  ];
+
+  # Environment variables
+  environment.variables.EDITOR = "neovim";
+
+  # Shell stuff
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
+
+  # Garbage collect old builds
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than +3";
+  };
+}
---
+++ nixos/modules/fhs-env.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, lib, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+
+    # Create an FHS environment using the command `fhs`, enabling the execution of non-NixOS packages in NixOS!
+    (let base = pkgs.appimageTools.defaultFhsEnvArgs; in
+      pkgs.buildFHSUserEnv (base // {
+      name = "fhs";
+      targetPkgs = pkgs: let
+        basePkgs = base.targetPkgs pkgs;
+      in
+        basePkgs ++ [
+          pkgs.pkg-config
+          pkgs.ncurses
+          pkgs.glibc
+        ];
+      profile = "export FHS=1";
+      runScript = "zsh";
+      extraOutputsToInstall = ["dev"];
+    }))
+  ];
+}
