5342ecdf3088749c99dc2f38db86127186713bfa
mr-ema <ema.a.v.c2017@gmail.com>
2024-06-18 21:46:22 -0400

    updated raylib shell -> nixos

--- nixos/shells/raylib.nix
+++ nixos/shells/raylib.nix
@@ -1,22 +1,27 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    rustup
-    cmake
-    clang
-    pkg-config
-    wayland
-    glfw
-    libGL
-    emscripten
-    xorg.libXrandr
-    xorg.libXinerama
-    xorg.libXcursor
-    xorg.libXi
+let
+  devDeps = with pkgs; builtins.concatLists [
+    [ alsa-lib libxkbcommon mesa libGL wayland ]
+
+    (with xorg; [
+      libX11
+      libXcursor
+      libXrandr
+      libXinerama
+      libXi
+      libXrender
+      libXext
+      libXfixes
+    ])
   ];
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ musl pkg-config rustup cmake emscripten clang glfw ];
+  buildInputs = with pkgs; [ (map (lib: lib.dev) devDeps) ];
 
   shellHook = ''
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/run/opengl-driver/lib:/run/opengl-driver/lib64
+    export CPATH=$(echo ${pkgs.lib.concatStringsSep ":" (map (dep: "${dep.dev}/include") devDeps)})
   '';
 }
