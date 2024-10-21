44f6153bf277387e0b714696bf70e2d8e3b78510
mr-ema <ema.a.v.c2017@gmail.com>
2024-10-21 11:55:48 -0300

    fix: godot lsp -> nvim

--- config/nvim/after/ftplugin/gdscript.lua
+++ config/nvim/after/ftplugin/gdscript.lua
@@ -1,12 +1,25 @@
 local port = os.getenv('GDScript_Port') or '6005'
-local cmd = vim.lsp.rpc.connect('127.0.0.1', port)
-local pipe = '/tmp/godot.pipe' -- I use /tmp/godot.pipe
+local pipe = '/tmp/godot.pipe' -- Godot pipe
 
-vim.lsp.start({
-  name = 'Godot',
-  cmd = cmd,
-  root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
-  on_attach = function(client, bufnr)
-    vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
-  end
-})
+local function start_godot_lsp(bufnr)
+  local cmd = { 'nc', '127.0.0.1', port }
+
+  -- Start LSP for the current buffer
+  vim.lsp.start({
+    name = 'Godot',
+    cmd = cmd,
+    root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
+    on_attach = function(client, bufnr)
+      -- Ensure server starts only once, but for multiple buffers
+      if not client.initialized then
+        vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
+        client.initialized = true
+      end
+    end,
+    -- Pass the current buffer as the context for the LSP client
+    bufnr = bufnr,
+  })
+end
+
+-- Attach LSP to the current buffer
+start_godot_lsp(vim.api.nvim_get_current_buf())
