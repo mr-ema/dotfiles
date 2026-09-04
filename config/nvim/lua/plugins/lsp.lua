e0d33f96d965d157a3856b3110e4b00eb55ff7dc
mr-ema <ema.a.v.c2017@gmail.com>
2026-09-04 13:50:49 -0400

    feat(neovim): add more lsp bindings

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -35,10 +35,11 @@ return {
                 remap = false,
             }
 
-            vim.keymap.set( "n", "<leader>dk", vim.diagnostic.goto_prev, opts)
-            vim.keymap.set( "n", "<leader>dj", vim.diagnostic.goto_next, opts)
+            vim.keymap.set( "n", "[d", vim.diagnostic.goto_prev, opts)
+            vim.keymap.set( "n", "]d", vim.diagnostic.goto_next, opts)
             vim.keymap.set( "n", "<space>dl", "<cmd>Telescope diagnostics<cr>", opts)
             vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+            vim.keymap.set("n", "<F3>", function() vim.lsp.buf.format({ async = true }) end, opts)
         end
 
         -- Common configuration for all LSP servers
