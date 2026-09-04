3bc77bd1d4ec69e0d4409d2a22f825349ffbdce1
mr-ema <ema.a.v.c2017@gmail.com>
2026-09-03 21:28:11 -0400

    feat(neovim): add tab binding lsp autocompletion

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -54,6 +54,9 @@ return {
                 ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
                 ["<C-y>"] = cmp.mapping.confirm({ select = true, }),
                 ["<C-Space>"] = cmp.mapping.complete(),
+
+                ["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
             },
 
             sources = {
